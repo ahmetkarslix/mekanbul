a002d0c2aede315f30511a3784f2ba0c5ee58d26
ahmetkarslix <aykar981@gmail.com>
2022-10-20 20:59:59 +0300

    Odev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,12 +1,81 @@
-var express = require('express');
-var router = express.Router();
-
 const anaSayfa = function (req, res) {
-    res.render('anasayfa', { title:'Anasayfa'});
+    res.render('anasayfa',
+
+     { "baslik":"Anasayfa",
+     "sayfaBaslik":{
+        "siteAd":"MekanBul",
+        "slogan":"Civardaki Mekanları Keşfet!"
+     },
+    "mekanlar":[
+       {
+        "ad":"Starbucks",
+        "puan":"5",
+        "adres":"Centrum Garden AWM",
+        "imkanlar":["Kahveler","Kurabiyeler","Pastalar"],
+        "mesafe":"3km"
+       },
+       {
+        "ad":"D'or Cafe",
+        "puan":"3",
+        "adres":"İyaşpark AVM Yanı",
+        "imkanlar":["Dünya Kahveleri","Kekler","Pastalar"],
+        "mesafe":"4km"
+       },
+       {
+        "ad":"Trigo Kafe",
+        "puan":"2",
+        "adres":"Modern Market Yanı",
+        "imkanlar":["Dünya Kahveleri","Dünya Mutfağı","İçecekler"],
+        "mesafe":"1km"
+       }
+     ]
+    });
 }
 
 const mekanBilgisi = function (req, res) {
-    res.render('mekanbilgisi', { title:'Mekan Bilgisi'});
+    res.render('mekanbilgisi',
+    {"baslik":"Mekan Bilgisi",
+    "mekanBaslik":"Starbucks",
+    "mekanDetay":{
+        "ad":"Starbucks",
+        "puan":"5",
+        "adres":"Centrum Garden AVM",
+        "saatler":[
+            {
+            "gunler":"Pazartesi-Cuma",
+            "acilis":"7:00",
+            "kapanis":"23.00",
+            "kapali": false
+            },
+            {
+            "gunler":"Cumartesi-Pazar",
+            "acilis":"8:00",
+            "kapanis":"22.00",
+            "kapali": false
+            }
+        ],
+        "imkanlar":["Kahveler","Kurabiyeler","Pastalar"],
+        "koordinatlar":{
+            "enlem":"37.78175551463006",
+            "boylam":"30.565071029913007"
+        },
+        "yorumlar":[
+            {
+            "yorumYapan":"Alperen Karslı",
+            "yorumMetni":"Kahveler güzel",
+            "tarih":"20 ekim 2022",
+            "puan":"4"
+            },
+            {
+                "yorumYapan":"Asım Sinan Yüksel",
+                "yorumMetni":"berbaaaat",
+                "tarih":"21 ekim 2022",
+                "puan":"1"
+                }
+        ],
+
+    }
+});
 }
 
 const yorumEkle = function (req, res) {
