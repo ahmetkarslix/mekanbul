d2e4b6c0acda965e81378e66cf9adae6430f7eac
ahmetkarslix <aykar981@gmail.com>
2022-11-26 00:11:35 +0300

    Odev6 Değişiklikleri

--- app_api/models/mekansema.js
+++ app_api/models/mekansema.js
@@ -1,14 +1,17 @@
 var mongoose = require( "mongoose" );
+
+
 var saatSema = new mongoose.Schema({
     gunler: { type: String, required: true},
     acilis: String,
     kapanis: String,
-    kapali: { type: Boolean },
+    kapali: { type: Boolean }
 });
 var yorumSema = new mongoose.Schema({
     yorumYapan : { type: String, required: true },
     puan: { type: Number, default: 0, min: 0, max: 5 },
-    yorumMetni: { type: Date, default: Date.now},
+    yorumMetni:{type: String, required:true},
+    tarih: { type: Date, default: Date.now}
 });
 var mekanSema = new mongoose.Schema({ 
     ad: {type: String, required: true},
