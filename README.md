e8bce37e57dbcefea39cd3d3df94fd3b427efabe
ahmetkarslix <aykar981@gmail.com>
2022-12-03 23:25:19 +0300

    odev7 değişiklikleri

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+# Mekanbul Rest Api Adresleri
+---
+1. Girilen Konum Civarındaki Mekanları Listele
+![](resimler/Mekan%20Listele.png)
+
+2. Mekanid ile Mekan Bilgilerini Getir
+![](resimler/Mekan%20Getir.png)
+
+3. Bir Mekana Ait Belirli Bir Yorumu Getir
+![](resimler/Yorum%20Getir.png)
+---
 # Api Collections Test
-![Collections Test](/resimler/Api%20Collections%20Test.png)
+![Collections Test](resimler/Api%20Collections%20Test.png)
 ---
 # Api Mocha Test
-![Mocha Test](/resimler/Api%20Mocha%20Test.png)
+![Collections Test](resimler/Api%20Mocha%20Test.png)
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,82 +1,93 @@
+const axios=require("axios");
+var apiSecenekleri = {
+    sunucu: "http://localhost:3000",
+    apiYolu: "/api/mekanlar/",
+};
+var mesafeyiFormatla=function(mesafe){
+    var yeniMesafe,birim;
+    if(mesafe > 1){
+        yeniMesafe = parseFloat(mesafe).toFixed(1);
+        birim =" km";
+    }else{
+        yeniMesafe = parseInt(mesafe*1000,10);
+        birim =" m";
+    }
+return yeniMesafe+birim;
+};
+var anaSayfaOlustur = function(res,mekanListesi){
+    var mesaj;
+    if(!(mekanListesi instanceof Array)){
+        mesaj="API HATASI:Bir şeyler ters gitti.";
+        mekanListesi=[];
+    }else{
+        if(!mekanListesi.length){
+            mesaj="Civarda herhangi bir mekan yok";
+        }
+    }
+    res.render("anasayfa",{
+        "baslik":"Anasayfa",
+        "sayfaBaslik":{
+            "siteAd":"Mekanbul",
+            "slogan":"Mekanları Keşfet!"
+        },
+        "mekanlar":mekanListesi,
+        "mesaj":mesaj
+    });
+}
 const anaSayfa = function (req, res) {
-    res.render('anasayfa',
-
-     { "baslik":"Anasayfa",
-     "sayfaBaslik":{
-        "siteAd":"MekanBul",
-        "slogan":"Civardaki Mekanları Keşfet!"
-     },
-    "mekanlar":[
-       {
-        "ad":"Starbucks",
-        "puan":"5",
-        "adres":"Centrum Garden AWM",
-        "imkanlar":["Kahveler","Kurabiyeler","Pastalar"],
-        "mesafe":"3km"
-       },
-       {
-        "ad":"D'or Cafe",
-        "puan":"3",
-        "adres":"İyaşpark AVM Yanı",
-        "imkanlar":["Dünya Kahveleri","Kekler","Pastalar"],
-        "mesafe":"4km"
-       },
-       {
-        "ad":"Trigo Kafe",
-        "puan":"2",
-        "adres":"Modern Market Yanı",
-        "imkanlar":["Dünya Kahveleri","Dünya Mutfağı","İçecekler"],
-        "mesafe":"1km"
-       }
-     ]
+    axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu,{
+        params:{
+            enlem:req.query.enlem,
+            boylam:req.query.boylam
+        }
+    }).then(function(response){
+        var i,mekanlar;
+        mekanlar=response.data;
+        for(i=0;i<mekanlar.length;i++){
+            mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
+        }
+        anaSayfaOlustur(res,mekanlar);
+    }).catch(function(hata){
+        anaSayfaOlustur(res,hata);
     });
 }
 
-const mekanBilgisi = function (req, res) {
-    res.render('mekanbilgisi',
-    {"baslik":"Mekan Bilgisi",
-    "mekanBaslik":"Starbucks",
-    "mekanDetay":{
-        "ad":"Starbucks",
-        "puan":"5",
-        "adres":"Centrum Garden AVM",
-        "saatler":[
-            {
-            "gunler":"Pazartesi-Cuma",
-            "acilis":"7:00",
-            "kapanis":"23.00",
-            "kapali": false
-            },
-            {
-            "gunler":"Cumartesi-Pazar",
-            "acilis":"8:00",
-            "kapanis":"22.00",
-            "kapali": false
-            }
-        ],
-        "imkanlar":["Kahveler","Kurabiyeler","Pastalar"],
-        "koordinatlar":{
-            "enlem":"37.8",
-            "boylam":"35.4"
-        },
-        "yorumlar":[
-            {
-            "yorumYapan":"Alperen Karslı",
-            "yorumMetni":"Kahveler güzel",
-            "tarih":"20 ekim 2022",
-            "puan":"4"
-            },
-            {
-                "yorumYapan":"Asım Sinan Yüksel",
-                "yorumMetni":"berbaaaat",
-                "tarih":"21 ekim 2022",
-                "puan":"1"
-                }
-        ],
-
+var detaySayfasiOlustur = function(res,mekanDetaylari){
+    mekanDetaylari.koordinat={
+        "enlem":mekanDetaylari.koordinat[0],
+        "boylam":mekanDetaylari.koordinat[1]
     }
-});
+    res.render('mekanbilgisi',
+    {
+        mekanBaslik: mekanDetaylari.ad,
+        mekanDetay: mekanDetaylari
+    });
 }
+
+var hataGoster = function (res, hata){
+    var mesaj;
+    if(hata.response.status==404){
+        mesaj ="404, Sayfa Bulunamadı!";
+    }
+    else {
+        mesaj=hata.response.status+" hatası";
+    }
+    res.status(hata.response.status);
+    res.render('error',{
+        "mesaj": mesaj
+    });
+};
+
+const mekanBilgisi = function (req, res) {
+   axios
+        .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+        .then(function (response) {
+            detaySayfasiOlustur(res, response.data);
+        })
+        .catch(function (hata) {
+            hataGoster(res, hata);
+        });
+};
 
 const yorumEkle = function (req, res) {
     res.render('yorumekle', { title:'Yorum Sayfası'});
